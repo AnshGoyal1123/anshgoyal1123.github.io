f1a92a73cfe308aa5d9f6222143aa2d174c2a37a
AnshGoyal1123 <AnshGoyal1123@users.noreply.github.com>
2025-10-13 18:35:03 +0000

    deploy: cec96ff0de50204b191e0917d079c07331c6e41b

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "AnshGoyal1123/anshgoyal1123.github.io",
+    "data-repo": "",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
